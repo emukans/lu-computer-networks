e69e6c38c3baddbd8306d1f0ef1f8628cad92655
emukans <mukans.work@gmail.com>
2018-12-23 23:05:26 +0200

    Describing the message send flow

--- md3/task1.py
+++ md3/task1.py
@@ -6,6 +6,7 @@
 
 import random
 import sys
+import hashlib
 from typing import Callable, Tuple
 
 sys.setrecursionlimit(100000)
@@ -76,28 +77,21 @@ def decrypt(private_key: Tuple[int, int], ciphertext: list) -> str:
     
     return ''.join(plain)
 
-# A list of prime numbers
-# [3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97
-# ,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179
-# ,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269
-# ,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367
-# ,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461
-# ,463,467,479,487,491,499,503,509,521,523,541,547,557,563,569,571
-# ,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,661
-# ,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773
-# ,787,797,809,811,821,823,827,829,839,853,857,859,863,877,881,883
-# ,887,907,911,919,929,937,941,947,953,967,971,977,983,991,997]
-
 public, private = generate_keypair(859, 733)
-print("Your public key is: ", public)
-print("Your private key is: ", private)
+print("Alise ģenerē publisko un privāto atslēgu pāri.")
+print("Alise dalās ar savu publisko atslēgu: ", public)
 
 message = 'KINO'
+message_hash = hashlib.md5(message.encode('utf-8')).hexdigest()
+print("Alise veido vēstules MD5 hešu un šifrē to ar privāto atslēgu.")
+print("Alise nosūta 2 vēstules Bobam.")
+print("Atklāta vēstule:", message)
 
-cipher = encrypt(public, message)
+cipher = encrypt(private, message_hash)
+print("Vēstules heša šifrs: ", ''.join(str(x) for x in cipher))
 
-print("Your encrypted message is: ")
-print(''.join(str(x) for x in cipher))
-print("Your message is:")
-print(decrypt(private, cipher))
+print("Bobs saņem 2 vēstules.")
+print("Bobs atšifrē vēstuli ar publisko atslēgu: ", decrypt(public, cipher))
+print("Bobs izveido atklāta vēstules MD5 hešu: ", message_hash)
+print("Bobs salīdzina vēstules hešu ar atšifrēto vēstuli. Ja tie sakrīt, sūtītājs un ziņojums ir pareizi.")
 
